d30deed902ba0483ebb7798eec29c729a4976468
Surajit Dhara <savvytech4@gmail.com>
2024-03-26 15:52:00 +0530

    Initial Commit

---
+++ NSxcframework/NSxcframework.h
@@ -0,0 +1,18 @@
+//
+//  NSxcframework.h
+//  NSxcframework
+//
+//  Created by Suraj on 25/03/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NSxcframework.
+FOUNDATION_EXPORT double NSxcframeworkVersionNumber;
+
+//! Project version string for NSxcframework.
+FOUNDATION_EXPORT const unsigned char NSxcframeworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NSxcframework/PublicHeader.h>
+
+
---
+++ NSxcframework/SimpleAlgorithms.swift
@@ -0,0 +1,38 @@
+//
+//  SimpleAlgorithms.swift
+//  NSxcframework
+//
+//  Created by Suraj on 25/03/24.
+//
+
+import Foundation
+public class SimpleAlgorithms {
+  
+    public static let sharedInstance = SimpleAlgorithms()
+    
+    private init(){
+        
+    }
+    
+    public func fibonacci(_ n: Int) -> Int {
+        var a = 1
+        var b = 1
+        guard n > 1 else { return a }
+
+        (2...n).forEach { _ in
+            (a, b) = (a + b, a)
+        }
+        return a
+    }
+
+    public func factorial(n: Int) -> Int {
+        var result = 1
+        if n > 0 {
+            (1...n).forEach { i in
+                result *= i
+            }
+        }
+        return result
+    }
+
+}
